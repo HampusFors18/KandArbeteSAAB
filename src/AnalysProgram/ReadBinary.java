aaab9d758ac4e1ab55fb33a20c073ea52010bf9b
Totte <Totte@130.238.250.6>
2018-03-28 17:11:45 +0200

    Detta har jag ändrat

--- src/AnalysProgram/ReadBinary.java
+++ src/AnalysProgram/ReadBinary.java
@@ -1,7 +1,34 @@
 package AnalysProgram;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ReadBinary {
-    //important comment
-	System.out.println("Important message")
-    private int importantVariable;
-}
+	public static void main (String [] args) {
+
+		try {
+			FileInputStream fileIs= new FileInputStream("exjobb871.raw");
+			BufferedInputStream in =new BufferedInputStream(fileIs);
+			
+			byte[] InData= new byte[100];
+			int BytesRead=0;
+			
+			in.read(InData,0,BytesRead);
+			
+			System.out.print(InData);
+			
+			in.close();
+			
+		}
+		catch (FileNotFoundException e1) {
+			System.err.println("Error - File not found");
+			
+		}
+		catch (IOException e2) {
+			System.err.println("Error - IO Exception");
+		}
+		
+	}
+	}
