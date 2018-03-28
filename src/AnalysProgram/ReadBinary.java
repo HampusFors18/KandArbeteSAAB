911df955f53806e761af4a1fa453cb7a4e71ccc5
EysiW <eystein@waade.net>
2018-03-28 21:14:12 +0200

    Fancy code with Simon

--- src/AnalysProgram/ReadBinary.java
+++ src/AnalysProgram/ReadBinary.java
@@ -4,6 +4,8 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class ReadBinary {
 	public static void main (String [] args) {
@@ -12,11 +14,23 @@ public class ReadBinary {
 			FileInputStream fileIs= new FileInputStream("exjobb871.raw");
 			BufferedInputStream in =new BufferedInputStream(fileIs);
 			
-			byte[] InData= new byte[100];
+			byte[] InData= new byte[49];
 			int BytesRead=0;
+
+			byte[] FSPEC = new byte[9];
+			in.read(FSPEC, 0, 9);
+
+			byte[] Range = new byte[2];
+			in.read(Range, 0, 2);
+
+			short r = ByteBuffer.wrap(Range).order(ByteOrder.LITTLE_ENDIAN).getShort();
+			System.out.println(r);
 			
-			in.read(InData,0,BytesRead);
-			
+			in.read(InData,0,49);
+
+			for(int i = 0; i < 49; i++) {
+				System.out.println(String.format("0x%02X", Byte.toUnsignedInt(InData[i])));
+			}
 			System.out.print(InData);
 			
 			in.close();
@@ -31,5 +45,5 @@ public class ReadBinary {
 		}
 		
 	}
-	}
+}
 
