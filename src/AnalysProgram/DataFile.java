aec2eda93113d84b625132b86c4d6bbb7e06cd4e
Totte <totte.malmgard@gmail.com>
2018-03-30 17:41:07 +0200

    tjoho new instancevariable

--- src/AnalysProgram/DataFile.java
+++ src/AnalysProgram/DataFile.java
@@ -3,9 +3,11 @@ package AnalysProgram;
 public class DataFile {
     public float azimuth;
     public float radius;
+    public float TottesDream;
 
     public DataFile(){
         this.azimuth = 0;
         this.radius = 0;
+        this.TottesDream = 0;
     }
 }
