16a4f6d72c0441eab7bbdb03cf61a85773b2230c
EysiW <eystein@waade.net>
2018-03-30 15:30:28 +0200

    Added radius instance variable

--- src/AnalysProgram/DataFile.java
+++ src/AnalysProgram/DataFile.java
@@ -2,8 +2,10 @@ package AnalysProgram;
 
 public class DataFile {
     public float azimuth;
+    public float radius;
 
     public DataFile(){
         this.azimuth = 0;
+        this.radius = 0;
     }
 }
